094be138b7b9501343e3d44ded59b45ec415f127
Hellder <desconfigurado@GGGGGmail.com>
2022-10-27 21:51:19 -0300

    Criando arquivo

---
+++ desafioFUDFNDF.js
@@ -0,0 +1,32 @@
+function soma(a, b){
+
+}
+
+function sub(a, b){
+
+}
+
+
+function div(a, b){
+    
+}
+
+
+function mult(a, b){
+    
+}
+
+
+function piso(a){
+    
+}
+
+
+function teto(a){
+    
+}
+
+
+
+
+

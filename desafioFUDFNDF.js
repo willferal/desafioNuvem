04bcce7d5a6380a4ab11a0fbbcba2a8bfe1ebd98
ALUNO K9 <alunok9@acad.unifor.br>
2022-10-27 22:06:05 -0300

    Arquivo Recebido e melhorado

--- desafioFUDFNDF.js
+++ desafioFUDFNDF.js
@@ -1,29 +1,32 @@
 function soma(a, b){
-
+return a + b;
 }
 
 function sub(a, b){
+    return a - b;
 
 }
 
 
 function div(a, b){
-    
+    return a / b;
 }
 
 
 function mult(a, b){
-    
+    return a * b;
+
 }
 
 
 function piso(a){
-    
+    return Math.trunc(a);
+
 }
 
 
 function teto(a){
-    
+    return Math.ceil(a);
 }
 
 
